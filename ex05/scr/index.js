ba416f2cc87c2013639ce95a0aca153dae60c6ad
Emir Selimovic <emirselimovic8@gmail.com>
2021-09-17 05:26:47 -0700

    travnik

---
+++ ex05/scr/index.js
@@ -0,0 +1,12 @@
+function main ()
+
+{
+    var myString="Concatention is the process of appending";
+    myString+="one string to the end of another string";
+    
+    return myString;
+
+}
+
+console.log(main());
+module.exports=main;
---
+++ ex06/src/index.js
@@ -0,0 +1,11 @@
+function main ()
+
+{
+var myName="Vahida";
+var myString="My name is "+myName+" and I am well!"
+return myString;
+
+}
+
+console.log(main());
+module.exports=main;
