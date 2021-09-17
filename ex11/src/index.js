aa42880f52f472e0f20dc32573cba082f90e6e06
Emir Selimovic <emirselimovic8@gmail.com>
2021-09-17 08:48:56 -0700

    travnicka princeza

---
+++ ex11/src/index.js
@@ -0,0 +1,16 @@
+function main ()
+
+{
+    var lastName="Tesla";
+
+            thirdLetterOfLastName=lastName[2];
+            
+
+    return thirdLetterOfLastName;
+
+
+
+}
+
+console.log(main());
+module.exports=main;
